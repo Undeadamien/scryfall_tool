43a531ac0614f8b4dea203b8783ea753ae397d10
undeadamien <damien.trala@outlook.com>
2025-06-21 15:59:40 +0200

    .

--- download.sh
+++ download.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
-LINK="https://data.scryfall.io/unique-artwork/unique-artwork-20250620090548.json"
-OUTPUT="unique_artwork.json"
+set -e
 
-curl "${LINK}" >"${OUTPUT}"
+cd "$(dirname "$0")"
+
+LINK_BULK_DATA="https://api.scryfall.com/bulk-data"
+RAW_BULK_DATA="$(curl "${LINK_BULK_DATA}")"
+
+SELECTED_TYPE="default_cards"
+LINK_SELECTED_TYPE="$(jq -r --arg type "${SELECTED_TYPE}" '.data | .[] | select(.type == $type) | .download_uri' <<<"${RAW_BULK_DATA}")"
+
+FILENAME_SELECTED_TYPE="${LINK_SELECTED_TYPE##*/}"
+
+echo "${LINK_SELECTED_TYPE}"
+echo "${FILENAME_SELECTED_TYPE}"
+
+if [ ! -f "${FILENAME_SELECTED_TYPE}" ]; then
+	find . -name "unique-artwork*.json" -delete
+	curl -v "${LINK_SELECTED_TYPE}" >"${FILENAME_SELECTED_TYPE}"
+fi
